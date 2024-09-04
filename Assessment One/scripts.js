d6b30309cd1a890170a5fbc6fff0a3551c0a3aa4
ynaiker <naiker.yevari@gmail.com>
2024-09-04 16:08:53 +1000

    Delete Assessment One directory

--- Assessment One/scripts.js
+++
@@ -1,5 +0,0 @@
-// Form submission alert
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form from submitting
-    alert('Form submitted!'); // Show alert on submission
-});
